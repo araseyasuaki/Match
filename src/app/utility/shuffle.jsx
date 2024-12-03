d023b66a78436332b39b5276bfdc586c437c21e6
Your Name <23aw0103@jec.ac.jp>
2024-12-04 00:12:07 +0900

    選択フォームテキストシャッフルシステム作成

---
+++ src/app/utility/shuffle.jsx
@@ -0,0 +1,34 @@
+export const formData1 = [
+  '簡潔',
+  'シンプル',
+  '動きのある',
+  '斬新な',
+  '洗練された',
+  '未来志向の',
+  '柔軟な',
+  'モダンな',
+  'インパクトのある',
+  '直感的な',
+];
+
+export const formData2 = [
+  'メンテナンス性に優れたコードを書く',
+  'ユーザー中心のデザインをする',
+  'リアルタイムで動作するアプリを開発する',
+  'セキュリティを強化する',
+  'スケーラブルなシステムを構築する',
+  'レスポンシブなUIを作る',
+  'アクセシビリティを考慮する',
+  '高速で動作するアプリを作る',
+  '持続可能なデザインを提案する',
+  'データ駆動型の機能を開発する',
+];
+
+export const shuffleForm = (data) => {
+  const shuffled = [...data];
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+  }
+  return shuffled;
+};
