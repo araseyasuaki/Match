7819c3994720ec887150caaf229f8cd1ea6708a3
Your Name <23aw0103@jec.ac.jp>
2024-12-04 00:12:46 +0900

    機能変更のため待機画面から学生欄を削除

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,12 +5,7 @@ import Link from 'next/link';
 const Home = () => {
   return (
     <div className='h-screen flex justify-center items-center gap-10'>
-      <div className='w-52 h-52 bg-red-400'>
-        <Link href='/student' className='w-full h-full flex justify-center items-center text-white'>
-          学生
-        </Link>
-      </div>
-      <div className='w-52 h-52 bg-red-400'>
+      <div className='w-52 h-32 bg-red-400 rounded-3xl'>
         <Link href='/matching' className='w-full h-full flex justify-center items-center text-white'>
           マッチング
         </Link>
